a1c1e045e6ac8f647bd13c7f179e38790bb5256f
suissa <jnascimento@gmail.com>
2025-07-28 14:26:29 -0300

    log data

--- src/application/controllers/pharmacy.controller.ts
+++ src/application/controllers/pharmacy.controller.ts
@@ -260,7 +260,7 @@ export class PharmacyControllerImpl implements PharmacyController {
               duration: 10000,
               delay: 10000,
             });
-            console.log("request.body?.data?.message", request.body?.data?.message);
+            console.log("request.body?.data", request.body?.data);
             const image = request.body?.data?.message?.base64;
             // console.log("request.body?.data?.message?.imageMessage", request.body?.data?.message?.imageMessage);
             // console.log("image", image);
