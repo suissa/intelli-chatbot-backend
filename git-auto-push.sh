02c762677bd140b92f861bf0f047bf0cd8a58d10
suissa <jnascimento@gmail.com>
2025-07-28 05:54:37 -0300

    corrigindo image

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -1,1 +1,12 @@
-git ac -m "corrigindo image" && gp
+#!/bin/bash
+
+# Adiciona tudo
+git add .
+
+# Comita se houver algo novo
+if ! git diff --cached --quiet; then
+  git commit -m "auto: save $(date '+%H:%M:%S')"
+  git push
+else
+  echo "[INFO] Nada para commitar."
+fi
