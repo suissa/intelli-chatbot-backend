f2f560e871da2a24ff737ce9e974137785fdc1d7
suissa <jnascimento@gmail.com>
2025-07-28 11:58:29 -0300

    vaii

--- src/application/controllers/pharmacy.controller.ts
+++ src/application/controllers/pharmacy.controller.ts
@@ -243,9 +243,9 @@ export class PharmacyControllerImpl implements PharmacyController {
               const pix = await this.drugImageProcessorService.processPixImage(imagePath);
               console.log("pix", pix);
               console.log("pix.pixInfo.valor", pix.pixInfo.valor);
-              const pixValue = (this.pixValue && typeof this.pixValue === 'number') ? this.pixValue : Number(this.pixValue?.toString().replace(',', '.'));
-              console.log("pixValue", pixValue);
-              if (Number(pixValue) === Number(pix.pixInfo.valor)) {
+              const pixValueImage = (this.pixValue && typeof this.pixValue === 'number') ? this.pixValue : Number(this.pixValue?.toString().replace(',', '.'));
+              console.log("pixValueImage", pixValueImage);
+              if (Number(pixValueImage) === Number(this.pixValue)) {
                 console.log("PIX PAGO CARAIIIII");
                 // history.push({ role: 'user', content: pixInfo.valor, name: 'user' }); // ✅ adiciona input do usuário
                 await client.messages.sendText({
