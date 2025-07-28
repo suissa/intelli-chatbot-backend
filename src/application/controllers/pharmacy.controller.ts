953257783f119e1d42a8b4559bd18dd22e7ee687
suissa <jnascimento@gmail.com>
2025-07-28 14:51:38 -0300

    log data

--- src/application/controllers/pharmacy.controller.ts
+++ src/application/controllers/pharmacy.controller.ts
@@ -220,7 +220,7 @@ export class PharmacyControllerImpl implements PharmacyController {
     try {
       // console.log(request.body);
       // const pharmacy = await this.pharmacyRepository.getPharmacyByCNPJ(cnpj);
-    
+      console.log("request.body", request.body);
       if (request.body?.event === "messages.upsert") {
         // console.log("request.body", request.body);
         // if (request.body?.data?.key?.fromMe === true) {
